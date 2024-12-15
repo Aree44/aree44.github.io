cfa931bfaec31c24a4df42a49122bbf8e1bb3434
Aree44 <64898419+Aree44@users.noreply.github.com>
2024-12-15 21:14:25 +0300

    Add files via upload

---
+++ FINAL/sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = "toxic-game-center-cache-v1";
+const urlsToCache = [
+    "/index.html",
+    "/styles.css",
+    "/script.js",
+    "/icons/icon-192x192.jpg",
+    "/icons/icon-512x512.jpg"
+];
+
+// Install the service worker and cache resources
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+// Serve cached resources
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+// Update the cache when needed
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
